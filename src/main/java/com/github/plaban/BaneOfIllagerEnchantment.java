ec4f083b2cc4ff10225a97bf613daf642dda393b
plaban <plabanmondal02@gmail.com>
2020-12-07 17:03:05 +0530

    Enchantment add: Bane of Illager
    Signed-off-by: plaban <plabanmondal02@gmail.com>

---
+++ src/main/java/com/github/plaban/BaneOfIllagerEnchantment.java
@@ -0,0 +1,53 @@
+package com.github.plaban;
+
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentTarget;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityGroup;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+
+public class BaneOfIllagerEnchantment extends Enchantment {
+    protected BaneOfIllagerEnchantment(Rarity weight, EnchantmentTarget type, EquipmentSlot[] slotTypes) {
+        super(weight, type, slotTypes);
+    }
+
+    @Override
+    public int getMinPower(int level) {
+        return 1 + 10 * (level - 1);
+    }
+
+    @Override
+    public int getMaxLevel() {
+        return 5;
+    }
+
+    @Override
+    public float getAttackDamage(int level, EntityGroup group) {
+        if(group == EntityGroup.ILLAGER) {
+            return 1.5f*level;
+        } else {
+            return 0f;
+        }
+    }
+
+    @Override
+    public void onTargetDamaged(LivingEntity user, Entity target, int level) {
+        if(target instanceof LivingEntity) {
+            if(((LivingEntity) target).getGroup() == EntityGroup.ILLAGER){
+                int time = 10 + user.getRandom().nextInt(10 * level);
+                ((LivingEntity) target).applyStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, time, 3));
+            }
+        }
+    }
+
+    @Override
+    public Text getName(int level) {
+        return super.getName(level).shallowCopy().formatted(Formatting.GREEN);
+    }
+}
--- src/main/java/com/github/plaban/MainFile.java
+++ src/main/java/com/github/plaban/MainFile.java
@@ -9,38 +9,46 @@ import net.minecraft.util.registry.Registry;
 
 public class MainFile implements ModInitializer {
 
-	public static final Enchantment COMBATHEAL = new CombatHealEnchantment(
-			Enchantment.Rarity.RARE,
-			EnchantmentTarget.WEAPON,
-			new EquipmentSlot[]{
-					EquipmentSlot.MAINHAND
-			}
-	);
+    public static final Enchantment COMBAT_HEAL = new CombatHealEnchantment(
+            Enchantment.Rarity.RARE,
+            EnchantmentTarget.WEAPON,
+            new EquipmentSlot[]{
+                    EquipmentSlot.MAINHAND
+            }
+    );
 
-	public static final Enchantment MAKEBOOM = new MakeBoomEnchantment(
-	        Enchantment.Rarity.UNCOMMON,
+    public static final Enchantment MAKE_BOOM = new MakeBoomEnchantment(
+            Enchantment.Rarity.RARE,
             EnchantmentTarget.TRIDENT,
             new EquipmentSlot[]{
                     EquipmentSlot.MAINHAND
             }
     );
 
-	public static final Enchantment POKE = new PokeEnchantment(
-			Enchantment.Rarity.UNCOMMON,
-			EnchantmentTarget.TRIDENT,
-			new EquipmentSlot[]{
-					EquipmentSlot.MAINHAND
-			}
-	);
+    public static final Enchantment POKE = new PokeEnchantment(
+            Enchantment.Rarity.UNCOMMON,
+            EnchantmentTarget.TRIDENT,
+            new EquipmentSlot[]{
+                    EquipmentSlot.MAINHAND
+            }
+    );
+
+    public static final Enchantment BANE_OF_ILLAGER = new BaneOfIllagerEnchantment(
+            Enchantment.Rarity.UNCOMMON,
+            EnchantmentTarget.WEAPON,
+            new EquipmentSlot[]{
+                    EquipmentSlot.MAINHAND
+            }
+    );
 
 
-
-	@Override
-	public void onInitialize() {
-		Registry.register(Registry.ENCHANTMENT, new Identifier("gg_enchantments", "combat_heal"), COMBATHEAL);
-	    Registry.register(Registry.ENCHANTMENT, new Identifier("gg_enchantments", "make_boom"), MAKEBOOM);
-		Registry.register(Registry.ENCHANTMENT, new Identifier("gg_enchantments", "poke"), POKE);
-	}
+    @Override
+    public void onInitialize() {
+        Registry.register(Registry.ENCHANTMENT, new Identifier("gg_enchantments", "combat_heal"), COMBAT_HEAL);
+        Registry.register(Registry.ENCHANTMENT, new Identifier("gg_enchantments", "make_boom"), MAKE_BOOM);
+        Registry.register(Registry.ENCHANTMENT, new Identifier("gg_enchantments", "poke"), POKE);
+        Registry.register(Registry.ENCHANTMENT, new Identifier("gg_enchantments", "bane_of_illager"), BANE_OF_ILLAGER);
+    }
 }
 
 
