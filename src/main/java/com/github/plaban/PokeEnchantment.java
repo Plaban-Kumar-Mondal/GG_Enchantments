5f147bc09f4ba10e47dda28d2e5b9231295109e3
plaban <plabanmondal02@gmail.com>
2020-12-07 15:46:13 +0530

    Enchantment text col set to GREEN
    Signed-off-by: plaban <plabanmondal02@gmail.com>

--- src/main/java/com/github/plaban/PokeEnchantment.java
+++ src/main/java/com/github/plaban/PokeEnchantment.java
@@ -5,6 +5,8 @@ import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentTarget;
 import net.minecraft.entity.EntityGroup;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 
 public class PokeEnchantment extends Enchantment {
     protected PokeEnchantment(Rarity weight, EnchantmentTarget type, EquipmentSlot[] slotTypes) {
@@ -26,5 +28,8 @@ public class PokeEnchantment extends Enchantment {
         return (1.5f*level)+0.5f;
     }
 
-
+    @Override
+    public Text getName(int level) {
+        return super.getName(level).shallowCopy().formatted(Formatting.GREEN);
+    }
 }
